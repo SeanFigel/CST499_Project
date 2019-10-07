b2d644ffe0f5eda2df1b1b048f02160955e962f6
SeanFigel <sfigel@csumb.edu>
2019-10-06 22:13:06 -0700

    Update routes.rb
    Updated routes for new databases

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,14 @@ Rails.application.routes.draw do
   resources :devices
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :employees
+  resources :notes
   
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
+  
+  #Redirect route
+  get "/app/views/home/index.html.erb", to:'home#index', as: 'viewHome'
 
 end
