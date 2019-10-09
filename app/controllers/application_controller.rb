c6fa8e2ddc5a9216de5d058efa9fb3d1d885a88e
Dennis Tang <detang@csumb.edu>
2019-10-09 01:45:40 +0000

    fixed back button bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user  
-  
+  before_filter :set_no_cache
   def current_user
     if session[:user_id]
       @current_user ||= User.find(session[:user_id])
     else
       @current_user = nil
     end
+  end
+
+
+  def set_no_cache
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
 end
